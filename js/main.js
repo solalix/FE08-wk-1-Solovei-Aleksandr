5f7f4d127100da46f80c9630582b5b2a0c8b21bf
solalix <solalix.s.a@gmail.com>
2021-02-28 12:08:15 +0300

    full-page-v1

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ $(function () {
       $('.menu-list').toggleClass('active');
    });
 
+   $('.tabs-btr-1').click(function(){
+      $('.slider-tabs__item').removeClass('active'),
+      $('.tabs-btr-1').addClass('active'),
+      $('.slider-content-item').removeClass('active'),
+      $('.slider-content-item-1').addClass('active');
+   });
+
+   $('.tabs-btr-2').click(function(){
+      $('.slider-tabs__item').removeClass('active'),
+      $('.tabs-btr-2').addClass('active'),
+      $('.slider-content-item').removeClass('active'),
+      $('.slider-content-item-2').addClass('active');
+   });
+
+   $('.tabs-btr-3').click(function(){
+      $('.slider-tabs__item').removeClass('active'),
+      $('.tabs-btr-3').addClass('active'),
+      $('.slider-content-item').removeClass('active'),
+      $('.slider-content-item-3').addClass('active');
+   });
+
+
 
 
 })
